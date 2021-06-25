5a17d651019a8fc868289f962b1d4ad6ca451874
Wilson Lin <code@wilsonl.in>
2021-06-26 06:45:59 +1000

    Improve metadata parsing

--- src/main.ts
+++ src/main.ts
@@ -104,7 +104,7 @@ export class Ff {
     )).trim();
 
     const properties = {} as MediaFileProperties;
-    for (const [_, sectionName, sectionData] of raw.matchAll(/\[([A-Z]+)]([^\[]*)\[\/\1]/g)) {
+    for (const [_, sectionName, sectionData] of raw.matchAll(/(^|\n)\[([A-Z]+)]([^\[]*)\n\[\/\2]/g)) {
       const values: { [key: string]: string } = Object.fromEntries(
         sectionData
           .trim()
